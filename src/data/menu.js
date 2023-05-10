443d2e43d59340203fc12b9a23169be876601b7a
Viacheslav Bedilo <viacheslav.bedilo@md.p2h.com>
2023-05-10 23:32:11 +0300

    Chore[Styling]: Fix styling and data import move

---
+++ src/data/menu.js
@@ -0,0 +1,20 @@
+const menu = [
+	{
+		url: "/",
+		text: "Home",
+	},
+	{
+		url: "/about",
+		text: "About",
+	},
+	{
+		url: "/blog",
+		text: "Blog",
+	},
+	{
+		url: "/404",
+		text: "Not Found",
+	},
+];
+
+export default menu;
